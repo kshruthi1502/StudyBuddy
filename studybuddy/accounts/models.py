df1d7c72c705e2317e46e4037396ca4a629aa53f
shravani-k <katepallishruthi@gmail.com>
2021-12-26 01:05:01 +0530

    added course table

--- studybuddy/accounts/models.py
+++ studybuddy/accounts/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+class Course(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    coursename = models.CharField(max_length=200)
